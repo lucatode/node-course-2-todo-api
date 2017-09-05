d85cf1c13101243ce0c46fd6d53fc5bffbfee207
DESKTOP-GFAGTVL\Luca <l.todeschini88@gmail.com>
2017-09-06 00:27:47 +0200

    add GET route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req,res) =>{
-    console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
@@ -21,6 +20,16 @@ app.post('/todos', (req,res) =>{
     });
 
 });
+
+app.get('/todos', (req, res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e)=>{
+        res.status(400).send(e);
+    });
+});
+
+
 
 app.listen(3000,() =>{
     console.log('Server up on localhost:3000');
